d203f0681f77b6e7eff19b7531a25142644b3b87
s12171934 <ssong99999@naver.com>
2024-05-29 21:58:52 +0900

    REFACTOR: remove unnecessary passport values

---
+++ src/main/java/com/solo/toyauthservice/dto/PassportDTO.java
@@ -0,0 +1,8 @@
+package com.solo.toyauthservice.dto;
+
+import lombok.Data;
+
+@Data
+public class PassportDTO {
+    private String username, role;
+}
--- src/main/java/com/solo/toyauthservice/service/PassportService.java
+++ src/main/java/com/solo/toyauthservice/service/PassportService.java
@@ -2,6 +2,7 @@ package com.solo.toyauthservice.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.solo.toyauthservice.dto.PassportDTO;
 import com.solo.toyauthservice.entity.UserEntity;
 import com.solo.toyauthservice.util.JWTUtil;
 import org.springframework.stereotype.Service;
@@ -18,14 +19,14 @@ public class PassportService {
 
     public String createPassport(String accessToken) throws JsonProcessingException {
 
-        UserEntity userEntity = new UserEntity();
+        PassportDTO passportDTO = new PassportDTO();
         String username = jwtUtil.getUsername(accessToken);
         String role = jwtUtil.getRole(accessToken);
 
-        userEntity.setUsername(username);
-        userEntity.setRole(role);
+        passportDTO.setUsername(username);
+        passportDTO.setRole(role);
 
         ObjectMapper objectMapper = new ObjectMapper();
-        return objectMapper.writeValueAsString(userEntity);
+        return objectMapper.writeValueAsString(passportDTO);
     }
 }
